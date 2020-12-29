ac4b35ab0e3ca64c640c98106485779ba2cc7960
gsus0194 <j.villegascambero@gmail.com>
2020-12-29 12:13:48 -0600

    [feat] add firebase to project

---
+++ src/utils/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDvqC0phB5Ztx4j_BxBTkJYF7a5JDg6tIY',
+  authDomain: 'birthday-194.firebaseapp.com',
+  projectId: 'birthday-194',
+  storageBucket: 'birthday-194.appspot.com',
+  messagingSenderId: '407374897175',
+  appId: '1:407374897175:web:3e2ad465b4d1c6001c3c52',
+};
+
+export default firebase.initializeApp(firebaseConfig);
